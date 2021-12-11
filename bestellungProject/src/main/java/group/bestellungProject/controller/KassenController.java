bdaea237337dff50ad1647f70c1640b3a1feffb5
thanhlongphan <thanhlong.phan92@gmail.com>
2021-12-12 00:22:00 +0100

    revert

---
+++ bestellungProject/src/main/java/group/bestellungProject/controller/KassenController.java
@@ -0,0 +1,11 @@
+package group.bestellungProject.controller;
+
+public class KassenController {
+
+	private KundenController kundenController;
+
+	private BestellungController bestellungController;
+
+	private ProduktController prodktController;
+
+}
---
+++ bestellungProject/src/main/java/group/bestellungProject/controller/ProduktController.java
@@ -0,0 +1,11 @@
+package group.bestellungProject.controller;
+
+public class ProduktController {
+
+	private KassenController kassenController;
+
+	public void produkHinzufügen() {
+
+	}
+
+}
