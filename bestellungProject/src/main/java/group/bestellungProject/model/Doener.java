d5cd3c8aa2682239379e8d0343e611ea68404843
thanhlongphan <thanhlong.phan92@gmail.com>
2021-12-11 23:44:38 +0100

    getter setter constructor

--- bestellungProject/src/main/java/group/bestellungProject/model/Doener.java
+++ bestellungProject/src/main/java/group/bestellungProject/model/Doener.java
@@ -3,4 +3,17 @@ public class Doener extends Produkt {
 
 	private Fleisch fleischSorte;
 
+
+	public Doener(int artikelNr, double preis, String artikelName, String beschreibung, Fleisch fleischSorte) {
+		super(artikelNr, preis, artikelName, beschreibung);
+		this.fleischSorte = fleischSorte;
+	}
+
+	public Fleisch getFleischSorte() {
+		return fleischSorte;
+	}
+
+	public void setFleischSorte(Fleisch fleischSorte) {
+		this.fleischSorte = fleischSorte;
+	}
 }
--- bestellungProject/src/main/java/group/bestellungProject/model/Pizza.java
+++ bestellungProject/src/main/java/group/bestellungProject/model/Pizza.java
@@ -3,4 +3,17 @@ public class Pizza extends Produkt {
 
 	private Size size;
 
+	public Pizza(int artikelNr, double preis, String artikelName, String beschreibung, Size size) {
+		super(artikelNr, preis, artikelName, beschreibung);
+		this.size = size;
+	}
+
+	public Size getSize() {
+		return size;
+	}
+
+	public void setSize(Size size) {
+		this.size = size;
+	}
+
 }
