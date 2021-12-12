612b099cfd2f98c5e98c1e6479fa155585f9fd93
Duc Chanh <duc-chanh.nguyen@tu-dortmund.de>
2021-12-12 01:52:51 +0100

    false data

--- bestellungProject/src/main/java/group/bestellungProject/controller/ProduktController.java
+++ bestellungProject/src/main/java/group/bestellungProject/controller/ProduktController.java
@@ -1,11 +1,26 @@
 package group.bestellungProject.controller;
 
+import java.util.List;
+
+import group.bestellungProject.model.Produkt;
+
 public class ProduktController {
 
 	private KassenController kassenController;
+	
 
-	public void produkHinzufügen() {
+	public void produktErstellen() {
 
 	}
+	public List<Produkt> sucheProdukt(String suchString){
+		return null;
+	}
+
+
+	public ProduktController(KassenController kassenController) {
+		
+		this.kassenController = kassenController;
+	}
+	
 
 }
