f04da0f0b8c3bd388f787e7126521b127a5b5a6d
Huyen Nguyen <huyen21195@gmail.com>
2021-12-13 00:58:41 +0100

    rest

--- bestellungProject/src/main/java/group/bestellungProject/model/Doener.java
+++ bestellungProject/src/main/java/group/bestellungProject/model/Doener.java
@@ -4,8 +4,8 @@ public class Doener extends Produkt {
 	private Fleisch fleischSorte;
 
 
-	public Doener(int artikelNr, double preis, String artikelName, String beschreibung, Fleisch fleischSorte) {
-		super(artikelNr, preis, artikelName, beschreibung);
+	public Doener(int id, double preis, String artikelName, String beschreibung, Fleisch fleischSorte) {
+		super(id, preis, artikelName, beschreibung);
 		this.fleischSorte = fleischSorte;
 	}
 
